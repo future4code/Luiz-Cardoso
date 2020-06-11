8acd2d55b775a67c4da2a0b459961073811659e2
luizbombonatti <lhbombonatti@gmail.com>
2020-06-11 19:07:48 -0300

    feito até exercício 4

--- semana4/funcoes-de-array/controlador-de-despesas/index.js
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -7,22 +7,51 @@ function limparDespesas(){
     select.value = ""
 }
 
-let arrayCadastrar = []
-
 const cadastrar = (limpar) => {
     let valor = document.getElementById("meuInput1")
     let descricao = document.getElementById("inputDescricao")
     let select = document.getElementById("meuSelect")
 
-    if(valor !== "" || descricao !== "" || select !== ""){
-        
+    if((valor.value !== "") && (descricao.value !== "") && (select.value !== "")){
+
         let infos = {
-            valor: valorDespesa,
-            tipo: select,
-            descricao: descricao
+            valor: valor.value,
+            tipo: select.value,
+            descricao: descricao.value
         }
         
+        
         arrayCadastrar.push(infos)
-        limparDespesas()
+        console.log(arrayCadastrar)
+        
+        
+        listaDespesas.innerHTML += `<h3>Despesa<h3> <p>Valor: ${valor.value}</p> <p>Tipo: ${select.value}</p> <p>Descrição: ${descricao.value}</p> `
     }
+    else{
+        alert("Campos Vazios")
+    }
+    
+    limparDespesas()
 }
+    let arrayCadastrar = []
+    let listaDespesas = document.getElementById("lista-despesa")
+
+const filtrar = () =>{
+    let valor2 = document.getElementById("meuSelect2")
+    let valorMax = document.getElementById("meuInput2")
+    let valorMin = document.getElementById("meuInput3")
+    let arrayDespesas = []
+    let listaFiltrada = []
+
+    
+    arrayDespesas = arrayCadastrar.filter((valor, idx, arr) =>{
+    return valor.tipo === valor2.value 
+     })
+     
+     console.log
+
+     if(valorMin.value < valorMax.valor && valorMax > valorMin.valor){
+        listaDespesas.innerHTML += 
+     }
+
+     }
