32ac34e891dd87c79396988f0009a343a8129ffd
luizbombonatti <lhbombonatti@gmail.com>
2020-06-12 14:00:17 -0300

    primeira sessão terminada

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,10 @@
+// exericio 1
+//R: Vai pegar o valor da função conversorDeMoeda e multiplicar pelo valor colocado no prompt
+
+//exercicio 2
+// R: A função pega os valores das variaveis da função e multiplica o valor dela com os case do switch, resultand no valor da multiplicação aplicada em cada case. 
+//Será exibido no console então, no primeiro caso, a case "Ações" com o resultado da multiplicação de 150 * 1.1, e no segundo caso, aparecerá a mensagem default, 
+//já que "Tesouro Direto", não é um dos case do switch. 
+
+//exercicio 3
+//
