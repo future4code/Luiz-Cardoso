af15f905899261bd2050f1fef0fba8df5375a551
luizbombonatti <lhbombonatti@gmail.com>
2020-06-15 15:09:16 -0300

    lista de exercicios completa

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -330,25 +330,25 @@
 
 //5. 
 
-const contas = [
-	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
-	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
-	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
-	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
-	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
-	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
-]
+// const contas = [
+// 	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+// 	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+// 	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+// 	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+// 	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+// 	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+// ]
 
-function atualizaSaldo(){
-    let atualiza = contas.forEach((e, i, arr)=>{
-        let c = 0
+// function atualizaSaldo(){
+//     let atualiza = contas.forEach((e, i, arr)=>{
+//         let c = 0
 
-        for(let i of e.compras){
-            c += i
-        }
-        e.saldoTotal = e.saldoTotal - c
-        c = 0 
-    })
-    return contas
-}
-console.log(atualizaSaldo(contas))
+//         for(let i of e.compras){
+//             c += i
+//         }
+//         e.saldoTotal = e.saldoTotal - c
+//         c = 0 
+//     })
+//     return contas
+// }
+// console.log(atualizaSaldo(contas))
